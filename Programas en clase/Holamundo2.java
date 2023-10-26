03b056f7e5c235655caafbc3fd1aa63825d0c415
davidethc <davidecondet@gmail.com>
2023-10-25 21:25:47 -0500

    Actualizo 25/10/23

---
+++ Programas en clase/Holamundo2.java
@@ -0,0 +1,6 @@
+public class Holamundo2 {
+	public static void main(String[] args) {
+		System.out.println("Holamundo");
+		System.out.println("Como estas hpy");
+	}
+}
---
+++ Programas en clase/Saludo1.java
@@ -0,0 +1,8 @@
+public class Saludo1{
+	public static void main(String[] args) {
+		String nombre;
+		nombre = "Davide";
+		System.out.println("saludos"+nombre+"!");
+	}
+
+}
